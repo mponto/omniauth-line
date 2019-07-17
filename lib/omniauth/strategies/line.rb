9d39a5a8466aa0c5dd42ac86322ed1b499ae0bed
Dylan Ang <40118518+shakuganang@users.noreply.github.com>
2019-07-17 14:24:59 +0900

    test

--- lib/omniauth/strategies/line.rb
+++ lib/omniauth/strategies/line.rb
@@ -5,7 +5,7 @@ module OmniAuth
   module Strategies
     class Line < OmniAuth::Strategies::OAuth2
       option :name, 'line'
-      option :scope, 'profile openid'
+      option :scope, 'profile test'
       option :bot_prompt, 'normal'
 
       option :client_options, {
